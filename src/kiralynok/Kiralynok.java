3f5320a42870c699f3419887a39e258a67e82284
varadizsolt <varadizsolt0212@gmail.com>
2021-02-17 10:38:22 +0100

    OOP 6.feladatig

--- src/kiralynok/Kiralynok.java
+++ src/kiralynok/Kiralynok.java
@@ -3,10 +3,36 @@ package kiralynok;
 import java.io.IOException;
 
 public class Kiralynok {
+    
+    private Tabla tabla;
+
+    public Kiralynok() {
+        tabla = new Tabla('#');
+        megoldasok();
+    }
+    
+    
+    
+    
     public static void main(String[] args) throws IOException {
+        Kiralynok kir = new Kiralynok();
+        kir.megoldasok();
+        
+        
+    }
+
+    public void megoldasok() {
+        feladat4();
+        feladat6();
+        }
+
+    private void feladat4() {
         System.out.println("4.feladat: Üres tábla létrehozása");
         Tabla tabla = new Tabla('#');
         tabla.megjelenit();
+    }
+
+    private void feladat6() {
         System.out.println("6.feladat: Feltöltött tábla");
         tabla.elhelyez(8);
         tabla.megjelenit();
