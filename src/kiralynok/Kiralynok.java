1bba321f83f7babcb8a639e0321e2ae1e773fae8
varadizsolt <varadizsolt0212@gmail.com>
2021-02-17 10:49:16 +0100

    OOP 9.feladatig

--- src/kiralynok/Kiralynok.java
+++ src/kiralynok/Kiralynok.java
@@ -24,18 +24,24 @@ public class Kiralynok {
     public void megoldasok() {
         feladat4();
         feladat6();
+        feladat9();
         }
 
     private void feladat4() {
-        System.out.println("4.feladat: Üres tábla létrehozása");
-        Tabla tabla = new Tabla('#');
+        System.out.println("4.feladat: Üres tábla létrehozása:");
         tabla.megjelenit();
     }
 
     private void feladat6() {
-        System.out.println("6.feladat: Feltöltött tábla");
+        System.out.println("6.feladat: Feltöltött tábla:");
         tabla.elhelyez(8);
         tabla.megjelenit();
+    }
+
+    private void feladat9() {
+        System.out.println("9. feladat: Üres oszlopok és sorok száma:");
+        System.out.println("sorok száma: " + tabla.uresSorokSzama());
+        System.out.println("oszlopok száma: " + tabla.uresOszlopokSzama());
     }
     
 }
